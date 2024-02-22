3eff73cec01bb8601567adf52d06283accc9ea57
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-22 18:18:31 +0600

    Fall 2021 number 5 A

--- Fall 2021/number_5_A.java
+++ Fall 2021/number_5_A.java
@@ -1,2 +1,27 @@
-class number_5_A {
+class Parent {
+    int i = 10;
+    public Parent(int j)
+    {
+        System.out.println(i); // 10
+        j = j*2;
+        this.i= j*10; // 80
+
+    }
+}
+
+
+class Child extends Parent {
+    public Child(int j)
+    {
+        super(j);
+        System.out.println(i); // 80
+        this.i = j * 20; // 80
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Child n = new Child(4);
+        System.out.println(n.i); // 80
+    }
 }
