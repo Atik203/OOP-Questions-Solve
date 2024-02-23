bdfa79981af5495d80fd5f52ac291d95cfbeb86c
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-23 17:42:17 +0600

    spring 2022 question !

---
+++ Spring 2022/number_1.java
@@ -0,0 +1,34 @@
+ class Marks {
+    String id ;
+    double quizMark;
+    double midMark;
+    double finalMark;
+
+    public Marks(String id, double quizMark, double midMark, double finalMark) {
+        this.id = id;
+        this.quizMark = quizMark;
+        this.midMark = midMark;
+        this.finalMark = finalMark;
+    }
+
+    public void passedOrNot()
+    {
+        double total = quizMark + midMark + finalMark;
+        if (total >= 55)
+        {
+            System.out.println("Passed");
+        }
+        else
+        {
+            System.out.println("Failed");
+        }
+    }
+
+}
+
+class Run{
+    public static void main(String[] args) {
+        Marks m1 = new Marks("1", 5, 10, 30);
+        m1.passedOrNot();
+    }
+}
---
+++ Spring 2022/number_2.java
@@ -0,0 +1,5 @@
+class number_2 {
+}
+
+class number_2_1 {
+}
