0f2f2362a59d37115825ad10e7dfcb69bee024aa
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-23 17:43:01 +0600

    spring 2022 question 1

--- Spring 2022/number_1.java
+++ Spring 2022/number_1.java
@@ -25,10 +25,11 @@
     }
 
 }
-
 class Run{
     public static void main(String[] args) {
         Marks m1 = new Marks("1", 5, 10, 30);
         m1.passedOrNot();
+        Marks m2 = new Marks("2", 15, 20, 30);
+        m2.passedOrNot();
     }
 }
