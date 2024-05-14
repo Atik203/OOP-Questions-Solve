5909a62e59030a5b8d6bc8dfcb66677237e5ee59
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-14 21:30:42 +0600

    Fall 2023 Final question 3-a

---
+++ Fall 2023 Final/Question_3_a.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Question_3_a extends JFrame implements ActionListener {
+    JButton addButton = new JButton("Add");
+    int count = 1;
+
+    public Question_3_a() {
+        super("Question 3 (a)");
+
+
+        addButton.addActionListener(this);
+        addButton.setBounds(50, 100, 95, 30);
+        add(addButton);
+
+        setLayout(new FlowLayout());
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Question_3_a();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == addButton)
+        {
+            JButton newButton = new JButton("Button " + count);
+            newButton.setBounds(50, 100, 95, 30);
+            add(newButton);
+            count++;
+            revalidate();
+            repaint();
+
+        }
+    }
+}
