886dee9842e9768caf842c7d425f7b245b62c6c0
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-15 21:34:19 +0600

    Spring 2023 Final Question 2-a

--- Spring 2023 Final/Question_2_a.java
+++ Spring 2023 Final/Question_2_a.java
@@ -1,2 +1,24 @@
-public class Question_2_a {
+
+class Math {
+    public int divide(int a, int b) {
+        return a / b;
+    }
 }
+
+ class test {
+    public static void main(String[] args) {
+        Math m = new Math();
+        int n = 4;
+        int d = 0;
+
+        try{
+            System.out.println(m.divide(n, d));
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed");
+        }finally {
+            System.out.println("Complete");
+        }
+
+    }
+}
+
