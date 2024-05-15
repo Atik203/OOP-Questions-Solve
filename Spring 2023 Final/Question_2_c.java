ca1d5a780e368a8127c021b4080be985401b0a12
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-15 21:50:32 +0600

    Spring 2023 Final Question 2-c

--- Spring 2023 Final/Question_2_c.java
+++ Spring 2023 Final/Question_2_c.java
@@ -1,2 +1,7 @@
-public class Question_2_c {
-}
+
+// what is call stack?
+
+/*
+* Call stack is a stack data structure that stores information about the active subroutines of a computer program.
+* When a program calls a subroutine, the subroutine is added to the top of the call stack.
+* */
