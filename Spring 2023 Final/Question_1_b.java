4b3b918037c8e346302ddda3a073ed118b504f55
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-15 21:30:57 +0600

    Spring 2023 Final Question 1-b

--- Spring 2023 Final/Question_1_b.java
+++ Spring 2023 Final/Question_1_b.java
@@ -1,2 +1,43 @@
-public class Question_1_b {
+
+
+interface A{
+    int p();
+    int q();
+}
+
+interface B{
+    int x();
+    int y();
+}
+
+abstract class C implements A, B{
+
+    abstract int z();
+}
+
+class D extends C{
+    public int p() {
+        System.out.println("p");
+        return 1;
+    }
+
+    public int q() {
+        System.out.println("q");
+        return 2;
+    }
+
+    public int x() {
+        System.out.println("x");
+        return 3;
+    }
+
+    public int y() {
+        System.out.println("y");
+        return 4;
+    }
+
+    public int z() {
+        System.out.println("z");
+        return 5;
+    }
 }
